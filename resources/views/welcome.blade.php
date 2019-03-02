93d9ade5d60d19369f12b26ed5124ad8d28da0c1
Admin <mido.b333@gmail.com>
2019-03-03 00:24:56 +0200

    Edit Project Name On Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Personal Blog
                 </div>
 
                 <div class="links">
